c2fc04cf1f43cdefe9432a8a5d4430bc16968f71
Catherine Kawara <ndutakawara2@gmail.com>
2022-03-22 20:05:52 +0300

    st: add form styling

--- js/home.js
+++ js/home.js
@@ -1,12 +1,15 @@
-const searchMeal = document.getElementById('button-addon2');
-searchMeal.addEventListener('click', getMeal);
+// const searchMeal = document.getElementById('button-addon2');
+// searchMeal.addEventListener('click', getMeal);
 
 
-function getMeal(){
-    let searchInput = document.getElementById('search').value.trim();
+
+    $('#button-addon2').click(function(){
+        let searchInput = $('#search').val();
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInput}`)
     .then(response => response.json())
     .then(data => {
         console.log(data)
     })
-}
+
+    })
+    
