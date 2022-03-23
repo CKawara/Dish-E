fb1eab3b1251326125e47650e8b7a383460161ad
Catherine Kawara <ndutakawara2@gmail.com>
2022-03-23 12:20:04 +0300

    ft: add feature to display not found message

--- js/home.js
+++ js/home.js
@@ -1,7 +1,4 @@
-// const searchMeal = document.getElementById('button-addon2');
-// searchMeal.addEventListener('click', getMeal);
-
-
+// get a list of meals that match the search results
 
     $('#button-addon2').click(function(){
         let searchInput = $('#search').val();
@@ -10,19 +7,27 @@
     .then(data => {
        if(data.meals){
            data.meals.forEach(meal =>{
-               $('.meal-results').append('<div class="col-lg-4 col-md-6 col-12 results">\
+               $('.meal-results').append('<div class="col-lg-4 col-md-6 col-12 result">\
                 <div class="card" style="width: 20rem;" data-id ="' + meal.idMeal + '")>\
                   <img src="' + meal.strMealThumb + '" class="card-img-top" alt="food img">\
                   <div class="card-body">\
                     <h5 class="card-title">' + meal.strMeal + '</h5>\
-                    <a href="#" class="btn">Open Recipe</a>\
+                    <a href="#" class="btn" id="get-recipe">Open Recipe</a>\
                   </div>\
                 </div>\
               </div>')
+            //   $('#button-addon2').hide();
            })
+           
        }else{
-           $('.meal-results').append("not found")
+           $('.not-found').show();
        }
+    });
+   
+    });
+  
+//Get recipe details 
+
+    $('#get-recipe').click(function(e){
+        e.preventDefault();
     })
-    })
-    
