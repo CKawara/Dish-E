6e8120f5695570d5300806646ad9102555701652
Catherine Kawara <ndutakawara2@gmail.com>
2022-03-23 08:42:18 +0300

    ft: add function to display search results on cards

--- js/home.js
+++ js/home.js
@@ -8,7 +8,21 @@
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInput}`)
     .then(response => response.json())
     .then(data => {
-        console.log(data)
+       if(data.meals){
+           data.meals.forEach(meal =>{
+               $('.meal-results').append('<div class="col-lg-4 col-md-6 col-12 results">\
+                <div class="card" style="width: 20rem;" data-id ="' + meal.idMeal + '")>\
+                  <img src="' + meal.strMealThumb + '" class="card-img-top" alt="food img">\
+                  <div class="card-body">\
+                    <h5 class="card-title">' + meal.strMeal + '</h5>\
+                    <a href="#" class="btn">Open Recipe</a>\
+                  </div>\
+                </div>\
+              </div>')
+           })
+       }else{
+           $('.meal-results').append("not found")
+       }
     })
     })
     
